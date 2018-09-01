920b1a139b0985a2b8c3759d9d1ae318dffdcea6
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-09-01 15:48:18 +0700

    Initial commit

---
+++ app/src/main/java/com/aar/app/apptuu/MainActivity.java
@@ -0,0 +1,46 @@
+package com.aar.app.apptuu;
+
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar mToolbar;
+    @BindView(R.id.drawerLayout) DrawerLayout mDrawerLayout;
+    @BindView(R.id.container) FrameLayout mContainer;
+    @BindView(R.id.navigationView) NavigationView mNavView;
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+
+        initNavDrawer();
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    private void initNavDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar,
+                R.string.open_drawer, R.string.close_drawer);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+    }
+}
